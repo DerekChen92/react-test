abbcc0bebae186bad8bbb5c12c5ece2ef175ba47
Derek.Chen <chingyuchen92@gmail.com>
2023-01-23 23:37:36 +0800

    Updates

---
+++ precache-manifest.ec22d9ec7d8f96276e4378dd06f92b4a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "81f35188015a6a9e3406",
+    "url": "/react-test/static/css/main.328d71a3.chunk.css"
+  },
+  {
+    "revision": "81f35188015a6a9e3406",
+    "url": "/react-test/static/js/main.81f35188.chunk.js"
+  },
+  {
+    "revision": "9af0e38fa19429b01f4f",
+    "url": "/react-test/static/js/1.9af0e38f.chunk.js"
+  },
+  {
+    "revision": "745cc6a50a47403619a0",
+    "url": "/react-test/static/js/runtime~main.745cc6a5.js"
+  },
+  {
+    "revision": "a08f3e2492955e71e888401d2496932e",
+    "url": "/react-test/index.html"
+  }
+];
